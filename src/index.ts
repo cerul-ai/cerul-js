4d0b457ee18ccd8dfc6518f627c1540dd1e8cacb
JessyTsui <jiaxicui446@gmail.com>
2026-04-04 15:16:28 +0800

    Bootstrap JavaScript SDK

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { cerul } from "./client.js";
+export { CerulError } from "./errors.js";
+export { SDK_VERSION } from "./version.js";
+export type {
+  CerulClient,
+  CerulOptions,
+  CreditBreakdown,
+  ExpiringCredit,
+  SearchFilters,
+  SearchRequest,
+  SearchResponse,
+  SearchResult,
+  UsageResponse
+} from "./types.js";
